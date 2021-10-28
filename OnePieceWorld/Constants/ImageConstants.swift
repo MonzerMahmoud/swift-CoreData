2b7a2ed4a1aba105ee7f152e34eeab657a02d7f6
MonzerMahmoud <55148623+MonzerMahmoud@users.noreply.github.com>
2021-10-28 19:50:58 +0200

    Add launch screen image

---
+++ OnePieceWorld/Constants/ImageConstants.swift
@@ -0,0 +1,13 @@
+//
+//  ImageConstants.swift
+//  OnePieceWorld
+//
+//  Created by Syber Expertise on 28/10/2021.
+//
+
+import Foundation
+
+class ImagesConstants {
+    
+    static let luffyBounty = "luffy_bounty"
+}
